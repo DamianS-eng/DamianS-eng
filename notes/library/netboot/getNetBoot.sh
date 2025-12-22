dc53ced8efd6249e08049cc376e313b79f300058
Damian <damiansuaz@gmail.com>
2025-12-22 13:59:02 -0500

    Fixes to script
    Based on learning about the quirks dealing with legacy and UEFI subsystems

--- notes/library/netboot/getNetBoot.sh
+++ notes/library/netboot/getNetBoot.sh
@@ -5,8 +5,12 @@ sudo wget $URL -O $boot_dir
 # FIXME
 # sudo vi /etc/grud.d/40_custom
 # add entry at end of file
+# ideally, parse the downloaded filename into the cfg
+# but be able to tell how GRUB loads, so that the cfg is set up correctly
+# adjust below if using UEFI versus legacy Boot, or if GRUB goes directly to /boot instead of /
 # menuentry "netboot.xyz" {
-#     linux16 /boot/netboot.xyz.lkrn
+#     linux16 /netboot.xyz.lkrn
+#     linuxefi /boot/netboot.xyz.lkrn  # use this for UEFI
 # }
 sudo update-grub # ubuntu
 sudo grub-2-mkconfig -o /boot/grub2/grub.cfg # fedora
