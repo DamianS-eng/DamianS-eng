33e9b47bb5d816e43f5f2b26924957f5db3feefb
Damian <damiansuaz@gmail.com>
2025-12-22 23:40:19 -0500

    Fix typos

--- notes/library/netboot/getNetBoot.sh
+++ notes/library/netboot/getNetBoot.sh
@@ -3,7 +3,7 @@ URL="https://boot.netboot.xyz/ipxe/netboot.xyz.lkrn"
 boot_dir="/boot/netboot.xyz.lkrn"
 sudo wget $URL -O $boot_dir
 # FIXME
-# sudo vi /etc/grud.d/40_custom
+# sudo vi /etc/grub.d/40_custom
 # add entry at end of file
 # ideally, parse the downloaded filename into the cfg
 # but be able to tell how GRUB loads, so that the cfg is set up correctly
@@ -13,4 +13,4 @@ sudo wget $URL -O $boot_dir
 #     linuxefi /boot/netboot.xyz.lkrn  # use this for UEFI
 # }
 sudo update-grub # ubuntu
-sudo grub-2-mkconfig -o /boot/grub2/grub.cfg # fedora
+sudo grub2-mkconfig -o /boot/grub2/grub.cfg # fedora
